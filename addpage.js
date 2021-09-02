828b994e1d2b908e8d54d9ea2f64f346ab43f2c6
kihunism <resacle93@naver.com>
2021-09-02 12:52:52 +0900

    nono

--- addpage.js
+++ addpage.js
@@ -3,6 +3,9 @@ let str1 = '변동사항 추가!';
 let str2 = '추가 말고 그냥 코드 변경'
 let number01 = 20210902;
 
+// 디자인 패턴 (Design Pattern)
+// 템플릿 메소드 패턴 (Template Method Pattern)
+
 function start() {
     printFunction(str);
     cut();
