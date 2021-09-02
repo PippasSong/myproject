a2df77108dd409b2e3813e2fd546cb0c3303462e
kihunism <resacle93@naver.com>
2021-09-02 10:05:17 +0900

    initial commit -> my first commit

---
+++ page.js
@@ -0,0 +1,20 @@
+let str = 'myproject 관리 git test 파일';
+let number01 = 20210902;
+
+function start() {
+    printFunction();
+    cut();
+    console.log(number01);
+}
+
+function cut() {
+    console.log('===============================')
+}
+
+function printFunction() {
+    console.log(str);
+}
+
+
+
+start();
