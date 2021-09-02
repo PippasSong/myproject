a544b3dec85a64dfe5a271ba606050d6b79528ba
kihunism <resacle93@naver.com>
2021-09-02 10:24:46 +0900

    changed

--- addpage.js
+++ addpage.js
@@ -1,13 +1,17 @@
 let str = 'myproject add(추가) git test 파일';
 let str1 = '변동사항 추가!';
+let str2 = '추가 말고 그냥 코드 변경'
 let number01 = 20210902;
 
 function start() {
     printFunction(str);
     cut();
-    console.log(number01);
+    printFunction(str1);
     cut();
-    console.log(str1);
+    printFunction(str2);
+    cut();
+
+    
 }
 
 function cut() {
